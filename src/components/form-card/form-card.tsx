a2b93b521e2030b0ca9faff47c0c802a622a90cb
Viktor Zhakhalov <viktorz@wix.com>
2022-04-05 11:07:03 +0300

    something meaningful

--- src/components/form-card/form-card.tsx
+++ src/components/form-card/form-card.tsx
@@ -1,7 +1,24 @@
+// Asset resolved to { package: 'react', file: './index.js' }
+// Types resolved to { package: '@types/react', file: './index.d.ts' }
 import React from 'react';
+
+// Asset resolved to { package: 'react', file: './index.js' }
+// Types resolved to { package: '@blueprintjs/core', file: '@blueprintjs/core.....index.d.ts' }
 import {Elevation, H1, TextArea, Button, Card, InputGroup, } from "@blueprintjs/core";
+
+// Asset resolved to { file: './src/components/assets/logo.svg' }
 import defaultLogoURL from './assets/logo.svg';
+
+// Asset resolved to { file: './src/components/form-card/form-card.scss' }
 import './form-card.css';
+
+// Files to fetch:
+// react/index.js and its recursive dependencies 
+// @types/react/index.d.ts and its recursive dependencies 
+// @blueprintjs/core.....index.d.ts and its recursive dependencies 
+// @blueprintjs/core.....index.d.ts and its recursive dependencies 
+// ./src/components/form-card/form-card.scss and its recursive dependencies 
+// ./src/components/assets/logo.svg
 
 
 export interface FormCardProps {        
