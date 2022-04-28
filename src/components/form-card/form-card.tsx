9e1939ba567f22ba231a461cbae410e8dfff4c04
Viktor Zhakhalov <viktorz@wix.com>
2022-04-28 12:46:57 +0300

    some change

--- src/components/form-card/form-card.tsx
+++ src/components/form-card/form-card.tsx
@@ -22,7 +22,7 @@ export const FormCard: React.FC<FormCardProps> = ({
 
 }) => {
     const submitCaption = formType === 'signin' ? 'Sign In' : formType === 'signup' ? 'Sign Up' : 'Send'
-    const formTitle = formType === 'signin' ? 'Sign In' : formType === 'signup' ? 'Sign Up' : 'Contact Us'
+    const formTitle = formType === 'signin' ? 'Sign In' : formType === 'signup' ? 'Sign Up' : 'Contact us'
 
     return (
         <Card className='card' elevation={Elevation.FOUR}>
