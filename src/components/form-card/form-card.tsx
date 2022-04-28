f048750e9f9d0eab6a4846c91d9061ffdfbd4264
Viktor Zhakhalov <viktorz@wix.com>
2022-04-28 13:44:54 +0300

    asdasd

--- src/components/form-card/form-card.tsx
+++ src/components/form-card/form-card.tsx
@@ -22,7 +22,7 @@ export const FormCard: React.FC<FormCardProps> = ({
 
 }) => {
     const submitCaption = formType === 'signin' ? 'Sign In' : formType === 'signup' ? 'Sign Up' : 'Send'
-    const formTitle = formType === 'signin' ? 'Sign In' : formType === 'signup' ? 'Sign Up' : 'Contact Uzzzzz'
+    const formTitle = formType === 'signin' ? 'Sign In' : formType === 'signup' ? 'Sign Up' : 'Contact Uz'
 
     return (
         <Card className='card' elevation={Elevation.FOUR}>
